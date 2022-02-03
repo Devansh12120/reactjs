ac5ff8dbd7a00a9489637074a6439ceb38df46bf
DEVANSH SHAH <56188496+Devansh12120@users.noreply.github.com>
2022-02-03 10:43:43 +0530

    updates todo app

--- src/ToDO.js
+++ src/ToDO.js
@@ -1,12 +1,13 @@
 import React from "react";
+import {DeleteRounded as DeleteIcon, Done} from '@material-ui/icons';
 const ToDO = (props) =>{
     
     return(
         <div className="todo_style">
             <div><li>{props.name}</li></div>
             <div className="icons">
-            <i className="fa fa-times" onClick={()=>{props.onSelect(props.id)}}></i>
-            <i className="fa fa-check" onClick={()=>{props.onComplete(props.id,props.name)}}></i>
+            <DeleteIcon className="fa fa-times" onClick={()=>{props.onSelect(props.id)}} />
+            <Done className="fa fa-check" onClick={()=>{props.onComplete(props.id,props.name)}}/>
             </div>
         </div>
     )
--- src/compleated.js
+++ src/compleated.js
@@ -1,11 +1,12 @@
 import React from "react";
+import {DeleteRounded as DeleteIcon, Done} from '@material-ui/icons';
 const Compleated = (props) =>{
     
     return(
         <div className="todo_style">
             <div><li>{props.name}</li></div>
             <div className="icons">
-            <i className="fa fa-times" onClick={()=>{props.onSelect(props.id)}}></i>
+            <DeleteIcon className="fa fa-times" onClick={()=>{props.onSelect(props.id)}} />
             </div>
         </div>
     )
